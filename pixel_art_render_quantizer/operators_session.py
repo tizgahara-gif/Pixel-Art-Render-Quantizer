4cc965b834364c9e263606cd2d3ab9b9174326ef
地図ヶ原 <tizgahara@yahoo.co.jp>
2026-07-04 16:59:47 +0900

    Fix palette sync during render panel draw

--- pixel_art_render_quantizer/operators_session.py
+++ pixel_art_render_quantizer/operators_session.py
@@ -1,6 +1,7 @@
 try: import bpy
 except ModuleNotFoundError: bpy=None
 from .palettes_builtin import DEFAULT_PALETTE_ID
+from .properties import sync_selected_palette_color
 
 if bpy:
  class PAQ_OT_start_pixel_render(bpy.types.Operator):
@@ -8,6 +9,7 @@ if bpy:
     def execute(self, context):
         s=context.scene; s.pixel_render_active=True; s.pixel_render_mode='ALL_IN_ONE'; s.pixel_render_look_palette_id=DEFAULT_PALETTE_ID; s.pixel_render_global_palette_id=DEFAULT_PALETTE_ID; s.pixel_render_background_palette_id=DEFAULT_PALETTE_ID
         s.pixel_render_width=320; s.pixel_render_height=180; s.pixel_render_scale='4'
+        sync_selected_palette_color(s)
         bpy.ops.paq.update_preview_nodes()
         return {'FINISHED'}
  class PAQ_OT_stop_pixel_render(bpy.types.Operator):
